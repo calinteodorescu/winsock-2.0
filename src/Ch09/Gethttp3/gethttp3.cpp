64a224b89ff443fa54e3cdb83458d74e334481cf
Calin Teodorescu <calinteodorescu@hotmail.com>
2022-12-07 16:08:10 -0500

    .

--- src/Ch09/Gethttp3/gethttp3.cpp
+++ src/Ch09/Gethttp3/gethttp3.cpp
@@ -1,8 +1,8 @@
 //
-// GetHTTP3.cpp --	Retrieve a file from a HTTP server
+// GetHTTP3.cpp --    Retrieve a file from a HTTP server
 //
-//					This version uses overlapped I/O
-//					with a completion function.
+//                    This version uses overlapped I/O
+//                    with a completion function.
 //
 // Compile and link with ws2_32.lib
 //
@@ -14,19 +14,22 @@
 
 #pragma comment(lib, "ws2_32.lib") // ws2_32 is the newer version, wsock32.lib is the obsolete version
 
-void GetHTTP(LPCSTR lpServerName, LPCSTR lpFileName);
+void GetHTTP( LPCSTR lpServerName,
+              LPCSTR lpFileName
+            );
 
 //
 // Overlapped I/O completion function
 //
-void CALLBACK RecvComplete(DWORD dwError, 
-						   DWORD cbTransferred, 
-						   LPWSAOVERLAPPED lpOverlapped, 
-						   DWORD dwFlags); 
+void CALLBACK RecvComplete( DWORD           dwError, 
+                            DWORD           cbTransferred, 
+                            LPWSAOVERLAPPED lpOverlapped, 
+                            DWORD           dwFlags
+                          );
 
 // Helper macro for displaying errors
-#define PRINTERROR(s)	\
-		fprintf(stderr,"\n%s %d\n", s, WSAGetLastError())
+#define PRINTERROR(s)    \
+        fprintf(stderr,"\n%s %d\n", s, WSAGetLastError())
 
 #define BUFFER_SIZE 1024
 
@@ -37,249 +40,314 @@ void CALLBACK RecvComplete(DWORD dwError,
 //
 typedef struct tagIOREQUEST
 {
-	WSAOVERLAPPED	over; // Must be first
-	SOCKET			Socket;
-	BOOL			fFinished;
-	LPBYTE			pBuffer;
-}IOREQUEST, *LPIOREQUEST;
+    WSAOVERLAPPED over; // Must be first
+    SOCKET        Socket;
+    BOOL          fFinished;
+    LPBYTE        pBuffer;
+} IOREQUEST, *LPIOREQUEST;
 
 
-int main(int argc, char **argv)
+int main( int argc, const char** argv )
 {
-	WORD wVersionRequested = WINSOCK_VERSION;
-	WSADATA wsaData;
-	int nRet;
+    WORD    wVersionRequested = WINSOCK_VERSION;
+    WSADATA wsaData           = {};
+    int     nRet              = -1;
 
-	//
-	// Check arguments
-	//
-	if (argc != 3)
-	{
-		fprintf(stderr,
-			"\nSyntax: GetHTTP ServerName FullPathName\n");
-		return 1;
-	}
+    //
+    // Check arguments
+    //
+    if ( argc != 3 )
+    {
+        fprintf( stderr, "\nSyntax: GetHTTP ServerName FullPathName\n" );
 
-	//
-	// Initialize WinSock.dll
-	//
-	nRet = WSAStartup(wVersionRequested, &wsaData);
-	if (nRet)
-	{
-		fprintf(stderr, "\nWSAStartup() error (%d)\n", 
-					nRet);
-		WSACleanup();
-		return 1;
-	}
+        return 1;
+    }
 
-	//
-	// Check WinSock version
-	//
-	if (wsaData.wVersion != wVersionRequested)
-	{
-		fprintf(stderr,"\nWinSock version not supported\n");
-		WSACleanup();
-		return 1;
-	}
+    //
+    // Initialize WinSock.dll
+    //
+    nRet = ::WSAStartup( wVersionRequested,
+                         & wsaData
+                       );
+    if ( nRet != 0 )
+    {
+        fprintf(stderr, "\nWSAStartup() error (%d)\n", nRet);
+        
+        ::WSACleanup();
 
-	//
-	// Set "stdout" to binary mode
-	// so that redirection will work
-	// for binary files (.gif, .jpg, .exe, etc.)
-	//
-	_setmode(_fileno(stdout), _O_BINARY);
+        return 1;
+    }
 
-	//
-	// Call GetHTTP() to do all the work
-	//
-	GetHTTP(argv[1], argv[2]);
+    //
+    // Check WinSock version
+    //
+    if ( wsaData.wVersion != wVersionRequested )
+    {
+        fprintf(stderr,"\nWinSock version not supported\n");
 
-	WSACleanup();
+        ::WSACleanup();
 
-	return 0;
+        return 1;
+    }
+
+    //
+    // Set "stdout" to binary mode
+    // so that redirection will work
+    // for binary files (.gif, .jpg, .exe, etc.)
+    //
+    _setmode(_fileno(stdout), _O_BINARY);
+
+    //
+    // Call GetHTTP() to do all the work
+    //
+
+    const char* serverName = argv[ 1 ];
+    const char* fileName   = argv[ 2 ];
+
+    GetHTTP( serverName,
+             fileName             
+           );
+
+    ::WSACleanup();
+
+    return 0;
 }
 
 
-void GetHTTP(LPCSTR lpServerName, LPCSTR lpFileName)
+void GetHTTP( LPCSTR lpServerName,
+              LPCSTR lpFileName
+            )
 {
-	LPHOSTENT	lpHostEntry;
-	SOCKADDR_IN saServer;
-	SOCKET		Socket;
-	int			nRet;
+    LPHOSTENT    lpHostEntry = nullptr;
+    SOCKADDR_IN  saServer    = {};
+    SOCKET       Socket      = INVALID_SOCKET;
+    int          nRet        = SOCKET_ERROR;
 
-	//
-	// Lookup the host address
-	//
-	lpHostEntry = gethostbyname(lpServerName);
-	if (lpHostEntry == NULL)
-	{
-		PRINTERROR("socket()");
-		return;
-	}
-	
-	// Create a TCP/IP stream socket
-	Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (Socket == INVALID_SOCKET)
-	{
-		PRINTERROR("socket()");
-		return;
-	}
+    //
+    // Lookup the host address
+    //
+    lpHostEntry = ::gethostbyname( lpServerName );
+    if ( lpHostEntry == NULL )
+    {
+        PRINTERROR("socket()");
 
-	//
-	// Fill in the rest of the server address structure
-	//
-	saServer.sin_family = AF_INET;
-	saServer.sin_addr = *((LPIN_ADDR)*lpHostEntry->h_addr_list);
-	saServer.sin_port = htons(80);
+        return;
+    }
+    
+    // Create a TCP/IP stream socket
+    Socket = ::socket( AF_INET,
+                       SOCK_STREAM,
+                       IPPROTO_TCP
+                     );
+    if ( Socket == INVALID_SOCKET )
+    {
+        PRINTERROR("socket()");
 
-	//
-	// Connect the socket
-	//
-	nRet = connect(Socket, (LPSOCKADDR)&saServer, sizeof(SOCKADDR_IN));
-	if (nRet == SOCKET_ERROR)
-	{
-		PRINTERROR("connect()");
-		closesocket(Socket);	
-		return;
-	}
-	
-	//
-	// Format the HTTP request
-	// and send it
-	//
-	char szBuffer[1024];
-	sprintf(szBuffer, "GET %s\n", lpFileName);
-	nRet = send(Socket, szBuffer, strlen(szBuffer), 0);
-	if (nRet == SOCKET_ERROR)
-	{
-		PRINTERROR("send()");
-		closesocket(Socket);	
-		return;
-	}
+        return;
+    }
 
-	//
-	// We're connected, so we can now
-	// post a receive buffer
-	//
+    //
+    // Fill in the rest of the server address structure
+    //
+    saServer.sin_family = AF_INET;
+    saServer.sin_addr   = * ( ( LPIN_ADDR ) * lpHostEntry->h_addr_list );
+    saServer.sin_port   = htons( 23456 );
 
-	// 
-	// We use a little trick here to pass additional
-	// information to out completion function.
-	// We append any additional info onto the end
-	// of a WSAOVERLAPPED structure and pass it
-	// as the lpOverlapped parameter to WSARecv()
-	// When the completion function is called,
-	// this additional info is then available.
-	//
-	BYTE aBuffer[BUFFER_SIZE];
-	IOREQUEST ioRequest;
-	memset(&ioRequest.over, 0, sizeof(WSAOVERLAPPED));
-	ioRequest.Socket = Socket;
-	ioRequest.fFinished = FALSE;
-	ioRequest.pBuffer = aBuffer;
+    //
+    // Connect the socket
+    //
+    nRet = ::connect( Socket,
+                      ( LPSOCKADDR ) & saServer,
+                      sizeof( SOCKADDR_IN )
+                    );
+    if ( nRet == SOCKET_ERROR )
+    {
+        PRINTERROR("connect()");
 
-	WSABUF wsabuf;
-	wsabuf.len = BUFFER_SIZE;
-	wsabuf.buf = (char *)aBuffer;
+        ::closesocket( Socket );  
 
-	DWORD dwRecv;
-	DWORD dwFlags = 0;
-	nRet = WSARecv(Socket, 
-				   &wsabuf,
-				   1,
-				   &dwRecv,
-				   &dwFlags,
-				   (LPWSAOVERLAPPED)&ioRequest,
-				   RecvComplete);
-	if (nRet == SOCKET_ERROR)
-	{
-		if (WSAGetLastError() != WSA_IO_PENDING)
-		{
-			PRINTERROR("WSARecv()");
-			closesocket(Socket);
-			return;
-		}
-	}
+        return;
+    }
+    
+    //
+    // Format the HTTP request
+    // and send it
+    //
+    char szBuffer[ BUFFER_SIZE ] = {};
 
-	// Receive the file contents and print to stdout
-	while(1)
-	{
-		//
-		// We could do other processing here
-		//
+    sprintf( szBuffer, "GET %s\n", lpFileName );
 
-		//
-		// Use the SleepEx() function to signal
-		// that we are in an altertable wait state
-		//
-		SleepEx(0, TRUE);
+    nRet = ::send( Socket,
+                   szBuffer,
+                   strlen( szBuffer ),
+                   0
+                 );
 
-		//
-		// If the completion function says we're finished
-		//
-		if (ioRequest.fFinished)
-			break;
-	}
-	closesocket(Socket);	
+    if ( nRet == SOCKET_ERROR )
+    {
+        PRINTERROR("send()");
+
+        ::closesocket(Socket);    
+
+        return;
+    }
+
+    //
+    // We're connected, so we can now
+    // post a receive buffer
+    //
+
+    // 
+    // We use a little trick here to pass additional
+    // information to out completion function.
+    // We append any additional info onto the end
+    // of a WSAOVERLAPPED structure and pass it
+    // as the lpOverlapped parameter to WSARecv()
+    // When the completion function is called,
+    // this additional info is then available.
+    //
+    BYTE      aBuffer[ BUFFER_SIZE ] = {};
+
+    IOREQUEST ioRequest              = {};    
+    memset( & ioRequest.over, 0, sizeof( ioRequest.over ) );
+        ioRequest.Socket    = Socket;
+        ioRequest.fFinished = FALSE;
+        ioRequest.pBuffer   = aBuffer;
+
+    WSABUF wsabuf;
+        wsabuf.buf = ( char* ) aBuffer;
+        wsabuf.len = sizeof( aBuffer );
+
+    DWORD dwRecv  = 0;
+    DWORD dwFlags = 0;
+
+    nRet = ::WSARecv( Socket, 
+                      & wsabuf,
+                      1,
+                      & dwRecv,
+                      & dwFlags,
+                      ( LPWSAOVERLAPPED ) & ioRequest,
+                      RecvComplete
+                    );
+
+    if ( nRet == SOCKET_ERROR )
+    {
+        auto error = ::WSAGetLastError( );
+        switch ( error )
+        {
+            case WSA_IO_PENDING:
+            {
+                goto keepGoing;
+            }
+            default:
+            {
+                PRINTERROR("WSARecv()");
+
+                closesocket(Socket);
+
+                return;
+            }
+        }
+    }
+
+keepGoing:
+
+    // Receive the file contents and print to stdout
+    for( ;; )
+    {
+        //
+        // We could do other processing here
+        //
+
+        //
+        // Use the SleepEx() function to signal
+        // that we are in an altertable wait state
+        //
+        ::SleepEx( 0, TRUE );
+
+        //
+        // If the completion function says we're finished
+        //
+        if ( ioRequest.fFinished )
+        {
+            break;
+        }
+    }
+
+    closesocket( Socket );
 }
 
-void CALLBACK RecvComplete(DWORD dwError, 
-						   DWORD cbRecv, 
-						   LPWSAOVERLAPPED lpOver, 
-						   DWORD dwFlags)
+void CALLBACK RecvComplete( DWORD           dwError, 
+                            DWORD           cbRecv, 
+                            LPWSAOVERLAPPED lpOver, 
+                            DWORD           dwFlags
+                          )
 {
-	//
-	// Check for errors
-	//
-	if (dwError)
-	{
-		fprintf(stderr,"\nRecvComplete() error: %ld", 
-						dwError);
-		return;
-	}
+    //
+    // Check for errors
+    //
+    if ( dwError != 0 )
+    {
+        fprintf(stderr,"\nRecvComplete() error: %ld", dwError );
 
-	LPIOREQUEST pReq = (LPIOREQUEST)lpOver;
+        return;
+    }
 
-	//
-	// If no error and no data returned,
-	// then the connection has been closed.
-	//	
-	if (cbRecv == 0)
-	{
-		pReq->fFinished = TRUE;
-		return;
-	}
+    LPIOREQUEST pReq = ( LPIOREQUEST ) lpOver;
 
-	fprintf(stderr,"\nRecvComplete(): %ld bytes received", cbRecv);
-	//
-	// Write the received data to stdout
-	//
-	fwrite(pReq->pBuffer, cbRecv, 1, stdout);
+    //
+    // If no error and no data returned,
+    // then the connection has been closed.
+    //    
+    if ( cbRecv == 0 )
+    {
+        pReq->fFinished = TRUE;
 
-	// 
-	// And then post the buffer to receive again
-	//
-	WSABUF wsabuf;
-	wsabuf.len = BUFFER_SIZE;
-	wsabuf.buf = (char *)pReq->pBuffer;
+        return;
+    }
 
-	DWORD dwRecv;
-	dwFlags = 0;
-	int nRet;
-	nRet = WSARecv(pReq->Socket, 
-				   &wsabuf,
-				   1,
-				   &dwRecv,
-				   &dwFlags,
-				   lpOver,
-				   RecvComplete);
-	if (nRet == SOCKET_ERROR)
-	{
-		if (WSAGetLastError() != WSA_IO_PENDING)
-		{
-			PRINTERROR("RePost with WSARecv()");
-			pReq->fFinished = TRUE;
-		}
-	}
+    fprintf(stderr,"\nRecvComplete(): %ld bytes received", cbRecv);
+    //
+    // Write the received data to stdout
+    //
+    fwrite(pReq->pBuffer, cbRecv, 1, stdout);
+
+    // 
+    // And then post the buffer to receive again
+    //
+    WSABUF wsabuf;
+        wsabuf.buf = ( char* ) pReq->pBuffer;
+        wsabuf.len = BUFFER_SIZE;
+
+    DWORD dwRecv = 0;
+    int   nRet   = SOCKET_ERROR;
+    
+    dwFlags = 0;
+
+    nRet = ::WSARecv( pReq->Socket, 
+                      & wsabuf,
+                      1,
+                      & dwRecv,
+                      & dwFlags,
+                      lpOver,
+                      RecvComplete
+                    );
+
+    auto error = ::WSAGetLastError();
+    switch ( error )
+    {
+        case WSA_IO_PENDING:
+        {
+            break;
+        }
+        default:
+        {
+            PRINTERROR("RePost with WSARecv()");
+
+            pReq->fFinished = TRUE;
+
+            break;
+        }
+    }
 }
 
